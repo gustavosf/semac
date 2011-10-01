92a92c5f4a21f128e45e4f3070928c0602d9f037
Gustavo <gustavosf@gmail.com>
2011-10-01 11:02:27 -0300

    Adicionado migration para a tabela de users

---
+++ fuel/app/migrations/001_create_users.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Fuel\Migrations;
+
+class Create_users {
+
+	public function up()
+	{
+		\DBUtil::create_table('users', array(
+			'id' => array('constraint' => 11, 'type' => 'int', 'auto_increment' => true),
+			'username' => array('constraint' => 255, 'type' => 'varchar'),
+			'password' => array('constraint' => 255, 'type' => 'varchar'),
+			'group' => array('constraint' => 11, 'type' => 'int'),
+			'email' => array('constraint' => 255, 'type' => 'varchar'),
+			'last_login' => array('constraint' => 25, 'type' => 'varchar'),
+			'login_hash' => array('constraint' => 255, 'type' => 'varchar'),
+			'profile_fields' => array('type' => 'text'),
+		), array('id'));
+		
+		// seta username e email como campos únicos (não aceitando duplicatas)
+		\DB::query('ALTER TABLE Persons ADD CONSTRAINT users_id UNIQUE (username, email)');
+	}
+
+	public function down()
+	{
+		\DBUtil::drop_table('users');
+	}
+}
