b8fd3740d34679cf6863cccb422821b5f11f7c55
Gustavo <gustavosf@gmail.com>
2011-10-14 20:17:35 -0300

    Corrigido alguns problemas, adicionado usuarios para testes na task initdb

--- fuel/app/tests/classes/controller/admin.php
+++ fuel/app/tests/classes/controller/admin.php
@@ -43,12 +43,9 @@ class Tests_Admin extends \PHPUnit_Extensions_SeleniumTestCase {
 
 	public function testLoginWithValidCredentials()
 	{
-		// deve-se criar um usuário para este teste
-		// email: valid@credential.com
-		// senha: validPassword
 		$this->open('http://semac/admin/login');
-		$this->type('css=form input[type=text][name=username]', 'valid@credential.com');
-		$this->type('css=form input[type=password][name=password]', 'validPassword');
+		$this->type('css=form input[type=text][name=username]', 'adm@test.mail');
+		$this->type('css=form input[type=password][name=password]', '12345');
 		$this->click('css=form input[type=submit]');
 
 		$this->waitForPageToLoad(3000);
