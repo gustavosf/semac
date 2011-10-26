d1e1c024b7c9311d57ba94756d586111264e3817
Gustavo <gustavosf@gmail.com>
2011-10-26 11:21:04 -0200

    Extendendo classe validation

---
+++ fuel/app/tests/classes/validation.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * Testes para a extensão da classe Validation
+ * 
+ * @group App
+ * @group Validation
+ */
+class Tests_Validation extends TestCase {
+
+	protected function setUp() {}
+
+	public function testDates() {}
+	public function testTimes() {}
+	public function testDateArray() {}
+	public function testTimeArray() {}
+	
+}
