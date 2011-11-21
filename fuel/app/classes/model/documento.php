09321eb08d0601ad975e4d720c65c6f4d09a4661
Gustavo <gustavosf@gmail.com>
2011-11-21 18:21:07 -0200

    Adicionada adicao/remocao de documento por parte do chair

--- fuel/app/classes/model/documento.php
+++ fuel/app/classes/model/documento.php
@@ -7,4 +7,20 @@ class Model_Documento extends Orm\Model {
 		'atividade' => array('key_from' => 'id_atividade'),
 	);
 
+	/* Métodos */
+	public static function add($doc) {
+		$doc = new Model_Documento;
+		$doc->titulo = $doc['titulo'];
+		$doc->descricao = $doc['descricao'];
+		$doc->id_atividade = $doc['atividade'];
+		$doc->arquivo = $doc['arquivo'];
+		$doc->save();
+		return $doc;
+	}
+
+	public function destroi() {
+		@unlink(DOCROOT.DS.'doc'.DS.$this->atividade->id.DS.$this->arquivo);
+		parent::delete();
+	}
+
 }
